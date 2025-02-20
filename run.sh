319eb34ab63399639406ce2a5ff549f2671b63ab
PentestPuppy <170636746+PentestPuppy@users.noreply.github.com>
2025-02-20 14:28:58 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Vars
-tmux_script='~/conf/tmux.sh'
-tmux_conf='~/conf/.tmux.conf'
-zsh_rc='~/conf.zshrc'
+tmux_script='/root/conf/tmux.sh'
+tmux_conf='/root/conf/.tmux.conf'
+zsh_rc='/root/conf/.zshrc'
 
 printf "\e[1;36m-- . -- . -- APT UPDATE/ UPGRADE \e[m"
 
@@ -13,13 +13,13 @@ apt upgrade && \
 
 printf "\e[1;36m-- . -- . -- MOVE CONF FILES TO ROOT \e[m"
 
-cp $tmux_conf ~/
-cp $zsh_rc ~/
+cp "$tmux_conf" '/root'
+cat "$zsh_rc" >> '/root/.zshrc'
 
 printf "\e[1;36m-- . -- . -- FINISHED APT, STARTING TMUX INSTALL \e[m"
 
 bash $tmux_script && \
 printf "\e[1;36m-- . -- . -- FINISHED TMUX INSTALL"
 
-source ~/.zshrc
+source '~/.zshrc'
 
