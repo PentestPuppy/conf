6c6881b7066c53bd3f1324319f0698c78034d90b
PentestPuppy <170636746+PentestPuppy@users.noreply.github.com>
2025-02-20 15:22:59 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ printf "\e[1;36m-- . -- . -- APT UPDATE/ UPGRADE \e[m"
 
 cd ~/ && \
 apt update -y && \
-apt upgrade && \
+apt upgrade && sleep 5
 
 printf "\e[1;36m-- . -- . -- MOVE CONF FILES TO ROOT \e[m"
 
@@ -24,10 +24,37 @@ printf "\e[1;36m-- . -- . -- FINISHED TMUX INSTALL"
 source '~/.zshrc'
 
 # Set up proj directories
+printf "\e[1;36m-- . -- . -- SETTING UP PROJECT DIRS \e[m"
 cd /root
 mkdir $1
 cd $1
 mkdir nmap recon web creds cloud
 
+printf "\e[1;36m-- . -- . -- INSTALLING TOOLS \e[m"
+# Install tools
+# CLOUD
+cd /root/$1/cloud
+git clone "https://github.com/initstring/cloud_enum.git" && sleep 3
+cd cloud_enum
+pip install -r requirements.txt && sleep 3
+
+# CREDS
+cd /root/$1/creds
+# trevorspray
+pip install trevorspray && sleep 3
+
+# credmaster
+git clone "https://github.com/knavesec/CredMaster.git" && sleep 3
+cd CredMaster
+pip install -r requirements.txt && sleep 3
+
+# RECON
+cd /root/$1/recon
+# harvester
+git clone https://github.com/laramies/theHarvester.git && sleep 3
+cd theHarvester
+pip install -r requirements.txt && sleep 3
+
+printf "\e[1;36m-- . -- . -- STARTING TMUX SESSION \e [m"
 # Create tmux session
 tmux new -t $1
